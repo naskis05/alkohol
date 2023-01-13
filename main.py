eac9da03fb0ac75b48f6852851300a21309b8502
Viesturs Lukaševics <61879312+naskis05@users.noreply.github.com>
2023-01-13 09:00:18 +0000

    strādā jautājumu pārvade

--- main.py
+++ main.py
@@ -5,13 +5,15 @@ x=0
 logs=Tk()
 logs.title('Alkohola tests')
 logs.geometry('640x480')
-a=Canvas(logs, width=640, height=480, bg='gray')
+a=Canvas(logs, width=640, height=480)
 a.place(x=0, y=0)
 #Sākuma ekrāns
 label = Label(logs, text="Tests par alkoholu",font=25)
 label.place(x=250, y=100, height=20, width=150)
 
 a1=IntVar()
+a2=IntVar()
+
 
 
 def pirmaisuzd():
@@ -23,6 +25,26 @@ def pirmaisuzd():
   R1.place(x=250, y=150)
   R2.place(x=250, y=200)
   R3.place(x=250, y=250)
+  def izdzestpirmo():
+    J1.place_forget()
+    R1.place_forget()
+    R2.place_forget()
+    R3.place_forget()
+    poga1.place_forget()
+    otraisuzd()
+  poga1=Button(logs, text='Nākamais jautājums', command=izdzestpirmo)
+  poga1.place(x=250, y=300)
+
+
+def otraisuzd():
+  J2=Label(logs, text='Kurā vietā Latvija ir alkoholisma ziņā Eiropas Savienībā?', font=15)
+  R4=Checkbutton(logs, text='1. vieta', variable = a2, onvalue=2, offvalue=0)
+  R5=Checkbutton(logs, text='2. vieta', variable = a2, onvalue=1, offvalue=0)
+  R6=Checkbutton(logs, text='3. vieta', variable = a2, onvalue=3, offvalue=0)
+  J2.place(x=100, y=100)
+  R4.place(x=250, y=150)
+  R5.place(x=250, y=200)
+  R6.place(x=250, y=250)  
 
 def navsākumaekrāns():
   label.place_forget()
